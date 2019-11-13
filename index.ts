7d7e90202d2a93437adaba5d0f5cb41d138f5004
Javier Rayon Martinez <jrayon@aguapuebla.mx>
2019-11-12 21:36:29 -0600

    primer commit rest serve

---
+++ index.ts
@@ -0,0 +1,18 @@
+import Server from "./Clases/Server";
+import { SERVER_PORT } from "./global/enviroments";
+import router from "./routes/router";
+import bodyParser, { json } from 'body-parser';
+import cors from 'cors';
+
+const server = new Server();
+//body-parser
+server.app.use(bodyParser.urlencoded({extended:true}));
+server.app.use(bodyParser.json());
+
+//CORS  
+server.app.use(cors({origin:true ,credentials:true}));
+//Rutas de servicios
+server.app.use('/',router);
+server.start ( ()=>{
+    console.log(`Servidor Corriendo en el puerto ${SERVER_PORT}`);
+})
---
+++ readme.md
@@ -0,0 +1,16 @@
+#Socket-server
+
+Este es la prueba con sockets.
+
+'''
+npm install
+'''
+Generar el DIST
+'''
+tsc-w
+´´´
+
+levantar el  servidor, cualquiera de estos dos comandos
+nodemon dist/
+node dist/
+´´´ 
